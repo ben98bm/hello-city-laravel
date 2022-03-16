39675e1d9d9be05e757f7f75b50f11b91214c08f
ben98bm <72571324+ben98bm@users.noreply.github.com>
2022-03-16 14:40:34 +0000

    Add proper title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,8 @@
 @extends('base')
 
+@section('title', 'About Us | Hello City')
+
+
 @section('content')
     <p>Built with &hearts; by les teachers du net.</p>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,10 @@
 @extends('base')
 
+<!-- @section('title', 'Hello City') -->
+
+
 @section('content')
-    <h1>Hello fronm senegal</h1>
+    <h1>Hello from senegal</h1>
 
     <p>Il est actuellement {{ date('h:i A')}}.</p>
 @endsection

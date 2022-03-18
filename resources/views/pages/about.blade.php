a3d0d449383e0f9cb6ee255324b61fd5fac614cc
ben98bm <72571324+ben98bm@users.noreply.github.com>
2022-03-18 15:19:47 +0000

    add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,6 +4,7 @@
 
 
 @section('content')
+    <img src="/images/patrouille.jpg" alt="capiung">
     <p>Built with &hearts; by les teachers du net.</p>
 
     <p><a href="/">Revenir à la page d'acceuil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,8 @@
 
 
 @section('content')
+
+    <img src="/images/patrouille.jpg" alt="images">
     <h1>Hello from senegal</h1>
 
     <p>Il est actuellement {{ date('h:i A')}}.</p>
